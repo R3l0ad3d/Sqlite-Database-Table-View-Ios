bfbc6c389ad2a759933483179cb4010db4076d59
Joy <joy@joys-mac-mini.local>
2018-09-10 18:07:20 +0600

    Initial Commit

---
+++ FirstTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstTask
+//
+//  Created by Joy on 10/9/18.
+//  Copyright © 2018 Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstTask
+//
+//  Created by Joy on 10/9/18.
+//  Copyright © 2018 Joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
